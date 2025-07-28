98e43731077fc6baf6d0d237b30903f44e5a4ad5
Everton Niero <everton3neuron@gmail.com>
2025-07-28 17:25:13 -0300

    componentes e routers prontos

---
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-certificado-form',
+  imports: [ PrimaryButtonComponent, SecondaryButtonComponent, FormsModule, CommonModule],
+  templateUrl: './certificado-form.component.html',
+  styleUrl: './certificado-form.component.css'
+})
+export class CertificadoFormComponent {
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React'];
+
+  campoInvalido(control: NgModel){
+    return control.invalid && control.touched
+  }
+
+  formValido(){
+    return this.atividades.length > 0 && this.nome.length > 0
+  }
+}
